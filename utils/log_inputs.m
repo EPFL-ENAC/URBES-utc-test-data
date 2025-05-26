36b942b06bd050454edf3553236b5ebeb7a6f5bc
Hugo Solleder <hugo.solleder@epfl.ch>
2025-05-26 13:31:16 +0200

    chore: limit data save to first function call
    Issue(s): #1

--- utils/log_inputs.m
+++ utils/log_inputs.m
@@ -1,4 +1,13 @@
 function log_inputs(funcName, inputs)
+    persistent loggedFunctions
+    if isempty(loggedFunctions)
+        loggedFunctions = containers.Map();
+    end
+    if isKey(loggedFunctions, funcName)
+        return;
+    end
+    loggedFunctions(funcName) = true;
+
     filename = fullfile('data/inputs', [funcName '.mat']);
     save(filename, 'inputs');
 end
--- utils/log_outputs.m
+++ utils/log_outputs.m
@@ -1,4 +1,13 @@
 function log_outputs(funcName, outputs)
+    persistent loggedFunctions
+    if isempty(loggedFunctions)
+        loggedFunctions = containers.Map();
+    end
+    if isKey(loggedFunctions, funcName)
+        return;
+    end
+    loggedFunctions(funcName) = true;
+    
     filename = fullfile('data/outputs', [funcName '.mat']);
     save(filename, 'outputs');
 end
